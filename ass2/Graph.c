9f71ad67e3db521453dbd5439e65da4121d0b9fd
WillLin12 <47729021+WillLin12@users.noreply.github.com>
2019-04-20 20:54:43 +1000

    Finished

--- ass2/Graph.c
+++ ass2/Graph.c
@@ -4,143 +4,162 @@
 #include <assert.h>
 #include "Graph.h"
 
-struct _adjListNode* createNode(Vertex v);
-List insertLL(List, int);
-List deleteLL(List, int);
-bool inLL(List, int);
-void freeLL(List);
-void showLL(List);
+struct GraphRep {
+    adjListNode** edges;
+    int nV;
+   	int nE;
+};
 
-typedef struct GraphRep *Graph{
-	_adjListNode *AdjList;
-	int nV;
-	int nE;
-} GraphRep;
-
-
-struct _adjListNode *createNode(Vertex v) {
-
-   struct _adjListNode *new = malloc(sizeof(struct _adjListNode));
-   new->w = v;
-	new->weight = 0;
-   new->next = NULL;
-   return new;
-}
-
-List insertLL(List L, int n) {
-   if (inLL(L, n))
-      return L;
-
-   // add new node at the beginning
-   Node *new = makeNode(n);
-   new->next = L;
-   return new;
-}
-
-List deleteLL(List L, int n) {
-   if (L == NULL)
-      return L;
-   if (L->v == n)
-      return L->next;
-
-   L->next = deleteLL(L->next, n);
-   return L;
-
-}
-
-bool inLL(List L, int n) {
-   if (L == NULL)
-      return false;
-   if (L->v == n)
-     return true;
-
-   return inLL(L->next, n);
-}
-
-void showLL(List L) {
-   if (L == NULL)
-      putchar('\n');
-   else {
-      printf("%d ", L->v);
-      showLL(L->next);
-   }
-}
-
-void freeLL(List L) {
-   if (L != NULL) {
-      freeLL(L->next);
-      free(L);
-   }
-}
+static AdjList newAdjListNode(Vertex v, int weight);
+static void showLL(adjListNode *L);
+static void freeLL(adjListNode *L);
+static bool inLL(adjListNode *L, Vertex n);
+static AdjList deleteLL(adjListNode *L, int n);
 
 Graph newGraph(int noNodes) {
-	
-	struct Graph g = malloc(sizeof(GraphRep)); 
-	g->nV = noNodes;
+    assert(noNodes != 0);
 
-	g->edges = malloc(noNodes * sizeof(_adjListNode *));
-	int i;
-   	for (i = 0; i < noNodes; i++)
-      g->edges[i] = NULL;
+    //create empty graph
+    Graph g = malloc(sizeof(struct GraphRep));
+    g->nV = noNodes;
+    g->nE = 0;
 
-	return g;
+    g->edges = malloc(noNodes *sizeof(adjListNode *));
+
+    // set all edges to empty
+    int i = 0;
+    while (i < noNodes) {
+        g->edges[i] = NULL;
+        i++;
+    }
+
+    return g;
 }
 
 int numVerticies(Graph g) {
-    
-	return nV;
+    return g->nV;
 }
 
 void  insertEdge(Graph g, Vertex src, Vertex dest, int weight) {
-	//create edge from src to dest
-    struct _adjListNode *new = createNode(dest);
-	new->weight;
-    new->next = g->edges[src];
-    graph->edges[src] = newNode;
-	g->nV++;
-
-	
+    assert(g != NULL);
+        //if edge isnt in graph
+        if (!inLL(g->edges[src], dest)) {
+            AdjList newNode = newAdjListNode(dest, weight);
+            newNode->weight = weight;
+            newNode->next = g->edges[src];
+            g->edges[src] = newNode;
+        
+            g->nE++;
+        }
 }
-
+    
 void  removeEdge(Graph g, Vertex src, Vertex dest) {
-	if (inLL(g->edges[src], dest)) {
-		g->edges[src] = deleteLL(g->edges[src], dest;
-	}
- 
+    assert(g != NULL);
+
+	if (inLL(g->edges[src], dest)) {   // edge e in graph
+      g->edges[src] = deleteLL(g->edges[src], dest);
+      g->nE--;
+   }
 }
 
 bool adjacent(Graph g, Vertex src, Vertex dest) {
-   assert (g != NULL);
-   return inLL(g->edges[src], dest);
+    assert(g != NULL);
+    return inLL(g->edges[src], dest);
 }
 
 AdjList outIncident(Graph g, Vertex v) {
-	return NULL;
+    assert(g != NULL);
+    if (g->edges[v] == NULL) {
+        return NULL;
+    }
+    else /*(g->edges[v] != NULL)*/ {
+        AdjList currP = g->edges[v];
+        return currP;
+    }
+
 }
 
 AdjList inIncident(Graph g, Vertex v) {
-	return NULL;
+    assert(g != NULL);
+
+    AdjList n = NULL;
+    int i = 0;
+    while (i < g->nV) {
+        if (inLL(g->edges[i], v)) {
+            if (n == NULL) {
+                n = newAdjListNode(i, 0);
+            } else {
+                n->next = newAdjListNode(i,0);
+            }
+        }
+        i ++;
+    }
+    return n; 
 }
 
 void  showGraph(Graph g) {
-	assert(g != NULL);
+    assert(g != NULL);
     int i;
 
     printf("Number of vertices: %d\n", g->nV);
     printf("Number of edges: %d\n", g->nE);
     for (i = 0; i < g->nV; i++) {
        printf("%d - ", i);
-       showLL(g->edges[i]);	
+       showLL(g->edges[i]); 
     }
-
+ 
 }
 
 void  freeGraph(Graph g) {
-	assert(g != NULL);
-   	int i;
+    assert(g != NULL);
+    int i;
 
-   	for (i = 0; i < g->nV; i++)
+    for (i = 0; i < g->nV; i++)
       freeLL(g->edges[i]);
 
-   	free(g);
+    free(g);
+}
+
+static AdjList newAdjListNode(Vertex v, int weight) {
+    AdjList n = malloc(sizeof (AdjList));
+    n->w = v;
+    n->weight = weight;
+    n->next = NULL;
+    return n;
+}
+
+static void showLL(adjListNode *L) {
+   if (L == NULL)
+      putchar('\n');
+   else {
+      printf("%d ", L->w);
+      showLL(L->next);
+   }
+}
+
+static void freeLL(adjListNode *L) {
+   if (L != NULL) {
+      freeLL(L->next);
+      free(L);
+   }
+}
+
+static bool inLL(adjListNode *L, Vertex n) {
+   if (L == NULL)
+      return false;
+   if (L->w == n)
+     return true;
+
+   return inLL(L->next, n);
+}
+
+static AdjList deleteLL(adjListNode *L, int n) {
+   if (L == NULL)
+      return L;
+   if (L->w == n)
+      return L->next;
+
+   L->next = deleteLL(L->next, n);
+   return L;
+
 }
