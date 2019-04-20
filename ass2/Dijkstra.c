564c6e130140b74b3f6f9fd8bac73df184e53268
z5206114 <z5206114@cse.unsw.edu.au>
2019-04-20 20:57:34 +1000

    small changes

--- ass2/Dijkstra.c
+++ ass2/Dijkstra.c
@@ -7,13 +7,13 @@
 
 
 ShortestPaths dijkstra(Graph g, Vertex v) {
-	ShortestPaths new = malloc(sizeof(ShortestPaths));
-	new.src = v;
-	new.noNodes = Graph->nV;
-	new.dist[] = {9999999};
-	new.dist[new.src] = 0;
-	new.pred = malloc(new.noNodes*sizeof(PredNode));
-	new.pred[] = {-1};
+	ShortestPaths *new = malloc(sizeof(ShortestPaths));
+	new->src	 = v;
+	new->noNodes = Graph->nV;
+	new->dist[] = {9999999};
+	new->dist[new.src] = 0;
+	new->pred = malloc(new.noNodes*sizeof(PredNode));
+	new->pred[] = {-1};
 
 	return new;
 }
