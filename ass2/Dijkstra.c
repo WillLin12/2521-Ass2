9d16fb9371dfb8979e9fdc9c8e0c208e90f5a898
z5206114 <z5206114@cse.unsw.edu.au>
2019-04-20 21:28:11 +1000

    updated

--- ass2/Dijkstra.c
+++ ass2/Dijkstra.c
@@ -5,15 +5,16 @@
 #include <assert.h>
 #include <stdio.h>
 
+static void Relaxation(ShortestPaths s, int v, int w, int weight);
 
 ShortestPaths dijkstra(Graph g, Vertex v) {
-	ShortestPaths *new = malloc(sizeof(ShortestPaths));
-	new->src	 = v;
-	new->noNodes = Graph->nV;
-	new->dist[] = {9999999};
-	new->dist[new.src] = 0;
-	new->pred = malloc(new.noNodes*sizeof(PredNode));
-	new->pred[] = {-1};
+	ShortestPaths new = malloc(sizeof(struct ShortestPaths));
+	new.src	 = v;
+	new.noNodes = Graph->nV;
+	new.dist[] = {9999999};
+	new.dist[new.src] = 0;
+	new.pred = malloc(new.noNodes*sizeof(PredNode));
+	new.pred[] = {-1};
 
 	return new;
 }
@@ -27,3 +28,11 @@ void  freeShortestPaths(ShortestPaths paths) {
 
 }
 
+static void Relaxation(ShortestPaths s, int v, int w, int weight) {
+	if (s.dist[v] + weight < s.dist[w]) {
+		s.dist[w] = s.dist[v] + weight;
+		s.pred[w] = v;
+		
+	}
+
+}
