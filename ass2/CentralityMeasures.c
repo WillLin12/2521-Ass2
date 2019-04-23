cfe86046fc1c4371942469e9f15b468c35105f60
connoryu873 <49704605+connoryu873@users.noreply.github.com>
2019-04-23 11:37:20 +1000

    Update CentralityMeasures.c

--- ass2/CentralityMeasures.c
+++ ass2/CentralityMeasures.c
@@ -5,17 +5,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static NodeValues newNode() {
+	NodeValues newNode = malloc(sizeof(NodeValues));
+	newNode->noNodes = 0 ;
+	newNode->values = 0;
+	return newNode;
+}
+
 NodeValues outDegreeCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
+	NodeValues myNode = newNode();
+	myNode->noNodes = numVertices(g);
+	myNode->values = malloc(noNode*sizeof (double);
+	int i = 0;
+	while (i < myNode->noNodes) {
+		Adjlist myList = outIncident (g, i);
+		while (myList != NULL ) {
+			myNode->values[i]++;
+			myList = myList->next;
+		}	
+		i++;
+	}
+	return myNode;
 }
-NodeValues inDegreeCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
+
+NodeValues inDegreeCentrality(Graph g) {
+	NodeValues myNode = newNode();
+	myNode->noNodes = numVertices(g);
+	myNode->values = malloc(noNode*sizeof (double);
+	int i = 0;
+	while (i < myNode->noNodes) {
+		Adjlist myList = inIncident (g, i);
+		while (myList != NULL ) {
+			myNode->values[i]++;
+			myList = myList->next;
+		}	
+		i++;
+	}
+	return myNode;
 }
+
 NodeValues degreeCentrality(Graph g) {
 	NodeValues throwAway = {0};
 	return throwAway;
+	//return inDegreeCentrality(g) + outDegreeCentrality(g);
 }
 
 NodeValues closenessCentrality(Graph g){
