f4a86559699c7bea66e54bdb9e73013eb4749ffa
connoryu873 <49704605+connoryu873@users.noreply.github.com>
2019-04-24 13:47:17 +1000

    Update CentralityMeasures.c

--- ass2/CentralityMeasures.c
+++ ass2/CentralityMeasures.c
@@ -1,55 +1,58 @@
-// Graph ADT interface for Ass2 (COMP2521)
+	// Graph ADT interface for Ass2 (COMP2521)
 #include "CentralityMeasures.h"
 #include "Dijkstra.h"
 #include "PQ.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-static NodeValues newNode() {
-	NodeValues newNode = malloc(sizeof(NodeValues));
-	newNode->noNodes = 0 ;
-	newNode->values = 0;
+static NodeValues *newNode(Graph g);
+
+static NodeValues *newNode(Graph g) {
+	NodeValues *newNode = malloc(sizeof(NodeValues));
+	newNode->noNodes = numVerticies(g);
+	newNode->values = malloc(newNode->noNodes*sizeof (double));
+	int i = 0;
+	while (i < newNode->noNodes) {
+		newNode->values[i] = 0;
+		i++;
+	} 
 	return newNode;
 }
 
-NodeValues outDegreeCentrality(Graph g){
-	NodeValues myNode = newNode();
-	myNode->noNodes = numVertices(g);
-	myNode->values = malloc(noNode*sizeof (double);
+NodeValues outDegreeCentrality(Graph g) {
+	NodeValues *myNode = newNode(g);
 	int i = 0;
 	while (i < myNode->noNodes) {
-		Adjlist myList = outIncident (g, i);
+		AdjList myList = outIncident (g, i);
 		while (myList != NULL ) {
-			myNode->values[i]++;
+			myNode->values[i] += 1;
 			myList = myList->next;
 		}	
 		i++;
 	}
-	return myNode;
+	return *myNode;
 }
 
 NodeValues inDegreeCentrality(Graph g) {
-	NodeValues myNode = newNode();
-	myNode->noNodes = numVertices(g);
-	myNode->values = malloc(noNode*sizeof (double);
+	NodeValues *myNode = newNode(g);
 	int i = 0;
 	while (i < myNode->noNodes) {
-		Adjlist myList = inIncident (g, i);
+		AdjList myList = inIncident (g, i);
 		while (myList != NULL ) {
-			myNode->values[i]++;
+			myNode->values[i] += 1;
 			myList = myList->next;
 		}	
 		i++;
 	}
-	return myNode;
+	return *myNode;
 }
 
 NodeValues degreeCentrality(Graph g) {
 	NodeValues *myNode = newNode(g);
 	int i = 0;
 	while (i < myNode->noNodes) {
-		Adjlist myList = inIncident (g, i);
-		Adjlist myListTwo = outIncident(g, i);
+		AdjList myList = inIncident (g, i);
+		AdjList myListTwo = outIncident(g, i);
 		while (myList != NULL) {
 			myNode->values[i]++;
 			myList = myList->next;
@@ -65,11 +68,38 @@ NodeValues degreeCentrality(Graph g) {
 }
 
 NodeValues closenessCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
+	NodeValues *myNode = newNode(g);
+	int i = 0;
+
+	while (i < myNode->noNodes) {
+		int n = 0;
+		AdjList myList = inIncident (g, i);
+		AdjList myListTwo = outIncident(g, i);
+		while (myList != NULL) {
+			n++;
+			myList = myList->next;
+		}
+		while (myListTwo != NULL) {
+			n++;
+			myListTwo = myListTwo->next;
+		}
+		ShortestPaths Distance = dijkstra(g, i);
+		int shortestDistance = 0;
+		int j = 0;
+
+		while (j < Distance.noNodes) {
+			shortestDistance = shortestDistance + Distance.dist[j];
+			j++;
+		}
+		
+		myNode->values[i] = ((n-1)/(numVerticies(g) - 1)) * ((n -1)/shortestDistance);
+		i++;
+
+	}
+	return *myNode;
 }
 
-NodeValues betweennessCentrality(Graph g){
+NodeValues betweennessCentrality(Graph g) {
 	NodeValues throwAway = {0};
 	return throwAway;
 }
@@ -81,8 +111,20 @@ NodeValues betweennessCentralityNormalised(Graph g){
 
 void showNodeValues(NodeValues values){
 
+for (int i = 0; i < values.noNodes; i++) {
+		printf("%d: %lf\n", i, values.values[i]);
+	}
+
+
 }
 
 void freeNodeValues(NodeValues values){
+	/*int i = 0;
 
+	while (i < values.noNodes) {
+		//values.values[i] = NULL;
+		free(values->[i]);
+		i++;
+	}
+	//free(values);*/
 }
